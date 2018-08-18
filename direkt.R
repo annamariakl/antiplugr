54086cc7a9a245725c690e6e2e1adadf873d897c
ClaiRe000 <mariaclaire.reiss@stud.uni-gttingen.de>
2018-08-18 17:00:10 +0200

    ZUsammenhängende Funktion erstellen

--- direkt.R
+++ direkt.R
@@ -10,7 +10,7 @@
 #Packages------------------------------
 install.packages("quanteda")
 install.packages("tm")
-
+stitch("direkt.R")
 #-------------------------------------------------------
 # Test Texte
 zit1 <-  "Nach Köhlmoos belegt 1Kön 12,25-30* eine Geschichtstheologie, 
@@ -29,72 +29,46 @@ beiden Bruderstaaten voneinander, der sie beide in die Katastrophe führt."
 # Köhlmoos, Melanie: Korrektes Zitieren, zuletzt geprüft am 13.08.2018.
 
 #tokens-------------------------
-tok_zit1 <- quanteda::tokens(zit1, remove_numbers = TRUE, 
-                         remove_punc = TRUE, remove_symbols = TRUE, remove_hyphens = TRUE, 
-                         remove_separators = TRUE)
 
-tok_orig1 <- quanteda::tokens(orig1, remove_numbers = TRUE, 
-                         remove_punc = TRUE, remove_symbols = TRUE, remove_hyphens = TRUE, 
-remove_separators = TRUE)
-# kann nicht als Vector ausgegeben werden
-rm(ok_orig1)
-
-# Anzeigen 
-tok_zit1
-vec_zit1 <- as.vector(tok_zit1)
-vec_orig1 <- as.vector(tok_orig1)
 
 # ------------mit tm package den corpus erstellen
 #myCorpus_orig <- tm::Corpus(tm::VectorSource(vec_orig1)) 
 #myCorpus_orig
 # sieht jedes wort als dokument: nicht richtig
 
+
+
+ directq <- function(zit1,orig1){
 myCorpus_orig <- tm::Corpus(tm::VectorSource(c(zit1,orig1))) # Corpus erstellt
-myCorpus_orig
-# funktioniert
 
 #stamming---------------------
-myCorpus_orig_st <- tm::tm_map(x= myCorpus_orig,FUN=tm::stemDocument)
-#print(myCorpus_orig[1]$content)
 myCorpus_orig_plain <- tm::tm_map(myCorpus_orig,tm::PlainTextDocument)
+
 # warnung kann ignoriert werden eventuell ein Dataframe mit 
 #df <- data.frame(doc_id = doc_ids, text = text, stringsAsFactors = FALSE)
 #df_corpus <- Corpus(DataframeSource(df))
 #inspect(df_corpus)
-print(myCorpus_orig_st[1]$content)
 # vielleicht mit plain
 myCorpus_orig_st2 <- tm::tm_map(myCorpus_orig_plain,tm::stemDocument)
-print(myCorpus_orig_st2[1]$content)
 # funktioniert
-str(myCorpus_orig_st2)
-myCorpus_orig_st2
+
 
 #--------Umwandlung in quanteda
  my_corpus_cuanteda <- quanteda::corpus(myCorpus_orig_st2)
 # tokenize------
-my_corpus_cuanteda
 quan_stam_tok<- quanteda::tokens(my_corpus_cuanteda,remove_punct=T)
-quan_stam_tok
 # funktioniert
 # Problem ein dokument heißt meta ein anders heißt meta
 
 # Vergleichen--------------
-str(quan_stam_tok$content)
-str(quan_stam_tok$meta)
 # beides sind Charactervectoren
-quan_stam_tok$content[1]# Auf einzelnen token zugreifen
+#quan_stam_tok$content[1]# Auf einzelnen token zugreifen
 # Funktion finden die Abfolgen in charaktervektoren vergleicht
-
-# erster Versuch match
-?match
-# test
-4:6 %in% 1:10
-# gibt keine position
+ }
 
 # funktion, die position der Übereinstimmung findet
 # was macht die which funktion, wenn keins übereinstimmt
-test1 <- c(3,4,5)
-any(test1==6)
+t
 # muss mit any getestet werden
 
 find.pos <- function (p,o){
@@ -111,7 +85,7 @@ find.pos <- function (p,o){
   }
   return(pos_o)
 }
-# test
-o <- c(4,4,4,3)
-p <- c(5,4,4,4)
-find.pos(p,o)
+
+# test#+o <- c(4,4,4,3)
+#p <- c(5,4,4,4)
+#find.pos(p,o)
